5c5ba058a08d88be8fd3bbaefc56e981e9615518
caspervanschooten <casper@webbakery.nl>
2022-08-18 11:32:00 +0200

    Update

--- footer.php
+++ footer.php
@@ -42,18 +42,36 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/aos.js"></script>
 <script>
-    window.addEventListener('DOMContentLoaded', function () {
 
+
+    $(function() {
         AOS.init({
-            once: true,
-            duration: 2000,
-            offset: 0,
-            useClassNames: false,
-            disableMutationObserver: false,
+            once: true
+        });
 
 
-        })
+        onElementHeightChange(document.body, function(){
+            AOS.refresh();
+        });
+
     });
+
+    function onElementHeightChange(elm, callback) {
+        var lastHeight = elm.clientHeight
+        var newHeight;
+
+        (function run() {
+            newHeight = elm.clientHeight;
+            if (lastHeight !== newHeight) callback();
+            lastHeight = newHeight;
+
+            if (elm.onElementHeightChangeTimer) {
+                clearTimeout(elm.onElementHeightChangeTimer);
+            }
+
+            elm.onElementHeightChangeTimer = setTimeout(run, 200);
+        })();
+    }
 
 </script>
 
